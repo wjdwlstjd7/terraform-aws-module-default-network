4dea5e3e3009782e4cc4fcd409db62e4e595fb6f
정진성 <jjung@hist.co.kr>
2026-05-19 09:13:16 +0900

    fix: Terrakube 연동을 위한 http backend 설정 추가

--- providers.tf
+++ providers.tf
@@ -1,16 +1,18 @@
 terraform {
+  backend "http" {}
+
   required_version = ">= 1.0.7"
   required_providers {
     aws = {
       version = ">= 3.55.0"
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
     }
   }
 }
 
 provider "aws" {
   # Default Region
-  region = var.aws_default_region
+  region     = var.aws_default_region
   access_key = var.aws_access_key
   secret_key = var.aws_secret_key
 }
